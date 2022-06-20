c49c7d87474a184356a1431dd1c00674e4b2c920
Stuart Gendron <stuart.gendron@pwc.com>
2022-06-20 13:27:05 -0700

    changed owner to editor

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "google_project_iam_member" "project" {
   for_each = var.users
 
   project = var.project
-  role    = "roles/owner"
+  role    = "roles/editor"
   member  = "user:${each.value}"
 }
